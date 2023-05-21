e7b91b70b6d6dd069aebf347bc550cda7a3913f3
Zachary Soja <zachary3003@gmail.com>
2023-05-21 23:57:34 +0200

    feature: add swagger configuration

---
+++ Instagram.Api/Infrastructure/SwaggerConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+namespace Instagram.Api.Infrastructure
+{
+    public static class SwaggerConfiguration
+    {
+        public static void ConfigureSwagger(this IServiceCollection collection)
+        {
+            collection.AddSwaggerGen(options =>
+            {
+                options.CustomSchemaIds(type => type.ToString());
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v2",
+                    Title = "Instagram",
+                    Description = ""
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+                options.IncludeXmlComments(xmlPath);
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
+        }
+    }
+}
