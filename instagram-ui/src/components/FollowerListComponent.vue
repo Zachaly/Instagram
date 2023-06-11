82266ad6b1ee647707259e5063984e85b5b03922
Zachary Soja <zachary3003@gmail.com>
2023-06-11 23:14:26 +0200

    feature: add display

---
+++ instagram-ui/src/components/FollowerListComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="box" style="min-height: 40vh; min-width: 30vh;">
+        <div class="has-text-centered">
+            <font-awesome-icon @click="$emit('close')" :icon="['fas', 'x']" />
+        </div>
+        <UserLinkComponent v-for="user in follows" :key="user.followedUserId + user.followingUserId"
+            :id="follower ? user.followingUserId : user.followedUserId" :nick-name="user.userName!" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import UserFollowModel from '@/models/UserFollowModel';
+import UserLinkComponent from './UserLinkComponent.vue';
+
+const props = defineProps<{
+    follows: UserFollowModel[],
+    follower: boolean
+}>()
+
+defineEmits(['close'])
+
+</script>
