659c91ec34b0d38971139a71b48fb59d329b56a0
Zachary Soja <zachary3003@gmail.com>
2023-06-13 12:34:21 +0200

    tests: add application unit tests

---
+++ Instagram.Tests.Unit/FactoryTests/PostLikeFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using Instagram.Application;
+using Instagram.Models.PostLike.Request;
+
+namespace Instagram.Tests.Unit.FactoryTests
+{
+    public class PostLikeFactoryTests
+    {
+        private readonly PostLikeFactory _factory;
+
+        public PostLikeFactoryTests()
+        {
+            _factory = new PostLikeFactory();
+        }
+
+        [Fact]
+        public void Create_CreatesProperEntity()
+        {
+            var request = new AddPostLikeRequest { PostId = 1, UserId = 2 };
+
+            var like = _factory.Create(request);
+
+            Assert.Equal(request.PostId, like.PostId);
+            Assert.Equal(request.UserId, like.UserId);
+        }
+    }
+}
