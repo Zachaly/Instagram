a68e9f03f27cf439271daccf3177f58463cc8431
Zachary Soja <zachary3003@gmail.com>
2023-06-16 19:29:49 +0200

    tests: add unit tests

---
+++ Instagram.Application/PostComment/PostCommentFactory.cs
@@ -0,0 +1,14 @@
+﻿using Instagram.Application.Abstraction;
+using Instagram.Domain.Entity;
+using Instagram.Models.PostComment.Request;
+
+namespace Instagram.Application
+{
+    public class PostCommentFactory : IPostCommentFactory
+    {
+        public PostComment Create(AddPostCommentRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
