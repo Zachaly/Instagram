3730a4012f65ef821b87b94d28f2ae5b495b59f3
Zachary Soja <zachary3003@gmail.com>
2023-07-03 19:11:02 +0200

    feat: add implementation

--- Instagram.Application/Auth/Command/GetCurrentUserQuery.cs
+++ Instagram.Application/Auth/Command/GetCurrentUserQuery.cs
@@ -3,6 +3,7 @@ using Instagram.Application.Auth.Abstraction;
 using Instagram.Database.Repository;
 using Instagram.Models.Response;
 using Instagram.Models.User;
+using Instagram.Models.UserClaim.Request;
 using MediatR;
 
 namespace Instagram.Application.Auth.Command
@@ -21,6 +22,8 @@ namespace Instagram.Application.Auth.Command
         private readonly IResponseFactory _responseFactory;
         private readonly IUserFactory _userFactory;
 
+        const string FailMessage = "User not found!";
+
         public GetCurrentUserHandler(IAuthService authService, IUserRepository userRepository,
             IUserClaimRepository userClaimRepository, IUserDataService userDataService,
             IResponseFactory responseFactory, IUserFactory userFactory)
@@ -33,9 +36,36 @@ namespace Instagram.Application.Auth.Command
             _userFactory = userFactory;
         }
 
-        public Task<DataResponseModel<LoginResponse>> Handle(GetCurrentUserQuery request, CancellationToken cancellationToken)
+        public async Task<DataResponseModel<LoginResponse>> Handle(GetCurrentUserQuery request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var id = await _userDataService.GetCurrentUserId();
+
+                if(!id.HasValue)
+                {
+                    return _responseFactory.CreateFailure<LoginResponse>(FailMessage);
+                }
+
+                var user = await _userRepository.GetEntityByIdAsync(id.Value);
+
+                if(user is null)
+                {
+                    return _responseFactory.CreateFailure<LoginResponse>(FailMessage);
+                }
+
+                var claims = await _userClaimRepository.GetEntitiesAsync(new GetUserClaimRequest { UserId = id });
+
+                var token = await _authService.GenerateTokenAsync(user, claims);
+
+                var response = _userFactory.CreateLoginResponse(id.Value, token, user.Email, claims);
+
+                return _responseFactory.CreateSuccess(response);
+            }
+            catch(Exception ex)
+            {
+                return _responseFactory.CreateFailure<LoginResponse>(ex.Message);
+            }
         }
     }
 }
