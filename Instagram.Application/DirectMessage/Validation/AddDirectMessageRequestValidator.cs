0d84c00383d7d4d1f60484706bbbf79aa93c56dc
Zachary Soja <zachary3003@gmail.com>
2023-07-13 23:19:30 +0200

    feat: add validators and tests

---
+++ Instagram.Application/DirectMessage/Validation/AddDirectMessageRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Instagram.Models.DirectMessage.Request;
+
+namespace Instagram.Application.Validation
+{
+    public class AddDirectMessageRequestValidator : AbstractValidator<AddDirectMessageRequest>
+    {
+        public AddDirectMessageRequestValidator()
+        {
+            RuleFor(r => r.SenderId).GreaterThan(0);
+
+            RuleFor(r => r.ReceiverId).GreaterThan(0);
+
+            RuleFor(r => r.Content)
+                .MinimumLength(1)
+                .MaximumLength(300);
+        }
+    }
+}
