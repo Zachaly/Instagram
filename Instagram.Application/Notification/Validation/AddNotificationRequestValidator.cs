25fc53925a0b0e4080d66b91f8d510ed0ce84bdb
Zachary Soja <zachary3003@gmail.com>
2023-07-21 19:40:35 +0200

    feat: add validators and tests

---
+++ Instagram.Application/Notification/Validation/AddNotificationRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Instagram.Models.Notification.Request;
+
+namespace Instagram.Application.Validation
+{
+    public class AddNotificationRequestValidator : AbstractValidator<AddNotificationRequest>
+    {
+        public AddNotificationRequestValidator()
+        {
+            RuleFor(r => r.UserId).GreaterThan(0);
+            RuleFor(r => r.Message).MinimumLength(1).MaximumLength(300);
+        }
+    }
+}
---
+++ Instagram.Tests.Unit/Validator/UpdateNotificationRequestValidatorTests.cs
@@ -0,0 +1,42 @@
+﻿using Instagram.Application.Validation;
+using Instagram.Models.Notification.Request;
+
+namespace Instagram.Tests.Unit.Validator
+{
+    public class UpdateNotificationRequestValidatorTests
+    {
+        private readonly UpdateNotificationRequestValidator _validator;
+
+        public UpdateNotificationRequestValidatorTests()
+        {
+            _validator = new UpdateNotificationRequestValidator();
+        }
+
+        [Fact]
+        public void ValidRequest_PassesValidation()
+        {
+            var request = new UpdateNotificationRequest
+            {
+                Id = 1,
+                IsRead = true,
+            };
+
+            var res = _validator.Validate(request);
+
+            Assert.True(res.IsValid);
+        }
+
+        [Fact]
+        public void InvalidUserId_DoesNotPassValidation()
+        {
+            var request = new UpdateNotificationRequest
+            {
+                Id = 0
+            };
+
+            var res = _validator.Validate(request);
+
+            Assert.False(res.IsValid);
+        }
+    }
+}
