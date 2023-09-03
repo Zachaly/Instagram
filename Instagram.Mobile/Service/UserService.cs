d4d47b25d6569120312183ac96892a7e9a487f5e
Zachary Soja <zachary3003@gmail.com>
2023-09-03 19:58:51 +0200

    Feat/#862k8xrh3 add mobile profile update (#44)
    * feat(Mobile): add updating profile info
    
    * feat(Mobile): add update profile validation display
    
    * feat(Mobile): add profile picture update

--- Instagram.Mobile/Service/UserService.cs
+++ Instagram.Mobile/Service/UserService.cs
@@ -3,6 +3,7 @@ using Instagram.Models.User;
 using Instagram.Models.User.Request;
 using Newtonsoft.Json;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 
 namespace Instagram.Mobile.Service
@@ -12,6 +13,8 @@ namespace Instagram.Mobile.Service
         Task RegisterAsync(RegisterRequest registerRequest);
         Task<UserModel> GetByIdAsync(long id);
         Task<IEnumerable<UserModel>> GetAsync(GetUserRequest request);
+        Task UpdateAsync(UpdateUserRequest request);
+        Task UpdateProfilePictureAsync(long userId, string filePath);
     }
 
     public class UserService : IUserService
@@ -56,6 +59,38 @@ namespace Instagram.Mobile.Service
             var response = await _httpClient.GetAsync(request.BuildQuery(Endpoint));
 
             return await response.Content.ReadFromJsonAsync<IEnumerable<UserModel>>();
+        }
+
+        public async Task UpdateAsync(UpdateUserRequest request)
+        {
+            var response = await _httpClient.PatchAsJsonAsync(Endpoint, request);
+
+            if(response.StatusCode == HttpStatusCode.BadRequest)
+            {
+                throw new InvalidRequestException(JsonConvert.DeserializeObject<ResponseModel>(await response.Content.ReadAsStringAsync()));
+            }
+        }
+
+        public async Task UpdateProfilePictureAsync(long userId, string filePath)
+        {
+            using var request = new MultipartFormDataContent
+            {
+                { new StringContent(userId.ToString()), "UserId" }
+            };
+
+            if(filePath is not null)
+            {
+                using var fileContent = new StreamContent(File.OpenRead(filePath));
+                fileContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+                {
+                    FileName = filePath,
+                    Name = "File"
+                };
+                fileContent.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+                request.Add(fileContent);
+            }
+            
+            await _httpClient.PatchAsync($"{Configuration.ApiUrl}image/profile", request);
         }
     }
 }
---
+++ Instagram.Mobile/ViewModel/UpdateProfilePageViewModel.cs
@@ -0,0 +1,93 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Instagram.Mobile.Service;
+using Instagram.Models.User.Request;
+
+namespace Instagram.Mobile.ViewModel
+{
+    public partial class UpdateProfilePageViewModel : ObservableObject
+    {
+        private readonly IAuthorizationService _authorizationService;
+        private readonly IUserService _userService;
+
+        [ObservableProperty]
+        private UpdateUserRequest _updateRequest = new UpdateUserRequest();
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNotLoading))]
+        private bool _isLoading = true;
+
+        [ObservableProperty]
+        private IDictionary<string, string[]> _validationErrors = null;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNewProfilePictureSelected))]
+        private string _newProfilePicture = null;
+
+        public string CurrentProfilePicture => $"{Configuration.ImageUrl}profile/{_authorizationService.UserData.UserId}";
+
+        public bool IsNotLoading => !IsLoading;
+
+        public bool IsNewProfilePictureSelected => NewProfilePicture is not null;
+
+        public UpdateProfilePageViewModel(IAuthorizationService authorizationService, IUserService userService)
+        {
+            _authorizationService = authorizationService;
+            _userService = userService;
+        }
+
+        [RelayCommand]
+        private async Task LoadUserDataAsync()
+        {
+            IsLoading = true;
+
+            var user = await _userService.GetByIdAsync(_authorizationService.UserData.UserId);
+
+            ValidationErrors = null;
+
+            UpdateRequest = new UpdateUserRequest
+            {
+                Id = user.Id,
+                Bio = user.Bio,
+                Name = user.Name,
+                Nickname = user.Nickname,
+            };
+
+            IsLoading = false;
+        }
+
+        [RelayCommand]
+        private async Task UpdateProfileAsync()
+        {
+            try
+            {
+                await _userService.UpdateAsync(UpdateRequest);
+                await Toast.Make("Profile updated").Show();
+                ValidationErrors = null;
+            }
+            catch(InvalidRequestException exception)
+            {
+                ValidationErrors = exception.Response.ValidationErrors;
+            }
+        }
+
+        [RelayCommand]
+        private async Task SelectPictureAsync()
+        {
+            var file = await MediaPicker.PickPhotoAsync();
+
+            if(file is not null)
+            {
+                NewProfilePicture = file.FullPath;
+            }
+        }
+
+        [RelayCommand]
+        private async Task UpdateProfilePictureAsync()
+        {
+            await _userService.UpdateProfilePictureAsync(_authorizationService.UserData.UserId, NewProfilePicture);
+            await Toast.Make("Picture updated").Show();
+        }
+    }
+}
